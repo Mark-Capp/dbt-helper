3216fb65941d0c6b7ca2a1d33e0eb49977dfc2f6
MarkDavies <mark.davies@cappfinity.com>
2025-08-15 17:00:30 +0100

    Blah

--- Jinja2/IntBlock.cs
+++ Jinja2/IntBlock.cs
@@ -6,43 +6,33 @@ public class IntBlock(int value) : ExpressionBlock, IAdditionOperators<IntBlock,
 {
     public override object GetValue(Context context) => value;
 
-    public object Add(Context context, ExpressionBlock right)
-    {
-        return right switch
+    public object Add(Context context, ExpressionBlock right) =>
+        right switch
         {
             IntBlock intBlock => this + intBlock,
             IdBlock idBlock => idBlock.Add(context, this),
-            StringBlock stringBlock => $"{GetValue(context)}{stringBlock.GetValue(context)}"
         };
-    }
-    
-    public object Sub(Context context, ExpressionBlock right)
-    {
-        return right switch
-        {
-            IntBlock intBlock => this - intBlock,
-            IdBlock idBlock => idBlock.Sub(context, this),
-        };
-    }
-    
-    public object Mul(Context context, ExpressionBlock right)
-    {
-        return right switch
-        {
-            IntBlock intBlock => this - intBlock,
-            IdBlock idBlock => idBlock.Sub(context, this),
-        };
-    }
-    
-    public object Div(Context context, ExpressionBlock right)
-    {
-        return right switch
-        {
-            IntBlock intBlock => this - intBlock,
-            IdBlock idBlock => idBlock.Sub(context, this),
-        };
-    }
 
+    public object Sub(Context context, ExpressionBlock right) =>
+        right switch
+        {
+            IntBlock intBlock => this - intBlock,
+            IdBlock idBlock => idBlock.Sub(context, this),
+        };
+
+    public object Mul(Context context, ExpressionBlock right) =>
+        right switch
+        {
+            IntBlock intBlock => this - intBlock,
+            IdBlock idBlock => idBlock.Mul(context, this),
+        };
+
+    public object Div(Context context, ExpressionBlock right) =>
+        right switch
+        {
+            IntBlock intBlock => this - intBlock,
+            IdBlock idBlock => idBlock.Div(context, this),
+        };
 
     public static int operator +(IntBlock left, IntBlock right)
     {
